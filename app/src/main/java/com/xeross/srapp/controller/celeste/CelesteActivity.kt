111e2a028a3b928ee377f855b774f7c06517cfac
XeeroSs <qmasini@gmail.com>
2022-02-07 15:22:12 +0100

    ADD Circle Image with border

--- app/src/main/java/com/xeross/srapp/controller/celeste/CelesteActivity.kt
+++ app/src/main/java/com/xeross/srapp/controller/celeste/CelesteActivity.kt
@@ -2,10 +2,13 @@ package com.xeross.srapp.controller.celeste
 
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.xeross.srapp.R
 import com.xeross.srapp.adapter.PageAdapter
 import com.xeross.srapp.base.BaseActivityOAuth
 import com.xeross.srapp.controller.celeste.level.*
+import kotlinx.android.synthetic.main.activity_celeste.*
 
 class CelesteActivity : BaseActivityOAuth() {
     
@@ -14,18 +17,8 @@ class CelesteActivity : BaseActivityOAuth() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        val viewPager2 = findViewById<ViewPager2>(R.id.view_pager_levels)
-        
-        val fragmentList = arrayListOf(ForsakenCityFragment.getInstance(),
-            OldCityFragment.getInstance(),
-            CelestialResortFragment.getInstance(),
-            GoldenRidgeFragment.getInstance(),
-            MirrorTempleAFragment.getInstance(),
-            MirrorTempleBFragment.getInstance(),
-            ReflectionFragment.getInstance(),
-            TheSummitFragment.getInstance(),
-            CelesteRunFragment.getInstance())
-        
-        viewPager2.adapter = PageAdapter(this, fragmentList)
+        // Set image to image header with glide. Also allows rounded image
+        Glide.with(this).load(R.drawable.im_celeste)
+            .transform(RoundedCorners(8)).into(activity_game_details_image_header)
     }
 }
--- app/src/main/java/com/xeross/srapp/controller/main/MainActivity.kt
+++ app/src/main/java/com/xeross/srapp/controller/main/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.xeross.srapp.controller.main
 
 import android.content.Intent
+import android.graphics.Color
+import android.view.View
+import android.view.WindowManager
+import android.widget.RelativeLayout
+import com.bumptech.glide.Glide
 import com.google.android.material.navigation.NavigationBarView
 import com.xeross.srapp.R
 import com.xeross.srapp.adapter.GameAdapter
@@ -9,7 +14,11 @@ import com.xeross.srapp.controller.celeste.CelesteActivity
 import com.xeross.srapp.listener.ClickListener
 import com.xeross.srapp.model.Game
 import com.xeross.srapp.model.SpeedrunType
+import kotlinx.android.synthetic.main.activity_celeste.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.activity_game_details_image_header
+import kotlinx.android.synthetic.main.activity_main.bottom_navigation_menu
+
 
 class MainActivity : BaseActivity(), ClickListener<Game> {
     
@@ -27,6 +36,42 @@ class MainActivity : BaseActivity(), ClickListener<Game> {
             //activity_main_recyclerview.setRecyclerViewAdapter(it, false)
         }
         getGames()
+        
+        handleUI()
+    }
+    
+    private fun handleUI() {
+        // Reduce size image based on content shape size for create a border
+        // Method View::post allows to call the Thread for UI
+        activity_game_details_image_header.post {
+        
+            val image = activity_game_details_image_header
+            val imageContent = activity_game_details_content_image_header
+        
+            // Get new height and width for image
+            val borderSize = resources.getDimension(R.dimen.header_image_border_size)
+            val height: Int = (imageContent.height - borderSize.toInt())
+            val width: Int = (imageContent.width - borderSize.toInt())
+        
+            // Set image size
+            val params: RelativeLayout.LayoutParams = RelativeLayout.LayoutParams(height, width)
+            image.layoutParams = params
+            image.requestLayout()
+        
+            // Set image to image header with glide. Also allows rounded image
+            // TODO("Images customs")
+            Glide.with(this).load(R.drawable.im_celeste)
+                .centerCrop() // scale image to fill the entire ImageView
+                .circleCrop().into(image)
+        }
+    
+        // Status bar transparent
+        window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            statusBarColor = Color.TRANSPARENT
+        }
     
         handleBottomNavigationMenu()
     }
@@ -35,6 +80,13 @@ class MainActivity : BaseActivity(), ClickListener<Game> {
         // unselected the first item (the first item is selected by default when the activity is created)
         bottom_navigation_menu.menu.getItem(0).isCheckable = false
         (bottom_navigation_menu as NavigationBarView).setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener {
+            // Test
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    val intent = Intent(this, CelesteActivity::class.java)
+                    startActivity(intent)
+                }
+            }
             // return false allows don't show color after selected item
             return@OnItemSelectedListener false
         })
